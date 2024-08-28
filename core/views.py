2053f19359da802f639d814da940450a24f8e55a
Saman Gautam <samangautam443@gmail.com>
2024-08-28 22:57:47 +0545

    Updated Admin Panel Front-End

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+# for admin panel
+def dashboard(request):
+    return render(request, 'admin/dashboard.html')
+
+def package(request):
+    return render(request, 'admin/package.html')
+
+def driver(request):
+    return render(request, 'admin/driver.html')
+
+def ticket(request):
+    return render(request, 'admin/ticket.html')
--- eurotravels/urls.py
+++ eurotravels/urls.py
@@ -1,22 +1,13 @@
-"""
-URL configuration for eurotravels project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from core.views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    # urls for admin panel
+    path("panel/", dashboard, name="dashboard"),
+    path("package/", package, name="package"),
+    path("driver/", driver, name="driver"),
+    path("air/ticketing", ticket, name="ticket"),
 ]
