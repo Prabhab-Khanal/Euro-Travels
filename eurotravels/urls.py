8d4d6dc53c79506f4a85dfcfb06837fb90886e5f
Saman Gautam <samangautam443@gmail.com>
2024-08-29 15:31:57 +0545

    Updated URL Paths

--- eurotravels/urls.py
+++ eurotravels/urls.py
@@ -6,8 +6,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
 
     # urls for admin panel
+    path("euro-tour/login/", adminlogin, name="admin-login"),
+    path("euro-tour/logout/", adminlogout, name="admin-logout"),
     path("panel/", dashboard, name="dashboard"),
     path("package/", package, name="package"),
     path("driver/", driver, name="driver"),
-    path("air/ticketing", ticket, name="ticket"),
+    path("air/ticketing/", ticket, name="ticket"),
 ]
