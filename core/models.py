137d25b5174935d6c89ef5cc3e483f50227e7477
Saman Gautam <samangautam443@gmail.com>
2024-09-01 23:52:41 +0545

    Changes Database

--- core/models.py
+++ core/models.py
@@ -6,7 +6,7 @@ class Package(models.Model):
     packageId = models.AutoField(primary_key=True)
     packagename = models.CharField(max_length=255, verbose_name="Package Name", null=True, blank=False, default="Blank")
     duration = models.CharField(max_length=200, verbose_name="Duration", null=True, blank=False, default="Blank")
-    dataoftour = models.DateField(verbose_name="Date of Tour", null=True, blank=False)
+    dateoftour = models.DateField(verbose_name="Date of Tour", null=True, blank=False)
     price = models.IntegerField(verbose_name="Price of Package", null=True, blank=False)
     packagedescription = models.CharField(max_length=10000, verbose_name="Package Description", null=True, blank=False, default="Blank")
     image = models.ImageField(upload_to="packages/images", verbose_name="Images", null=True, blank=False, default='')
