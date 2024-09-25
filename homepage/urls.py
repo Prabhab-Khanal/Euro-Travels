1458c187dfd33aeee568c2e09cb6c9277082f8f1
Prabhab Khanal <khanalprabhab8@gmail.com>
2024-09-25 21:56:15 +0545

    GG

--- homepage/urls.py
+++ homepage/urls.py
@@ -8,8 +8,20 @@ urlpatterns =[
     path('main/', views.main1, name="main"),
 
     # for contact me pages
+<<<<<<< Updated upstream
     path('contact/', views.contact_view, name="contact-me"),
     path('success/', views.success, name="success-page"),
     path('error/', views.error, name="error-page"),
     path('error/', views.email_non_exist, name="email-page"),
+=======
+    # path('contact/', views.contact_view, name="contact-me"),
+    # path('success/', views.success, name="success-page"),
+    # path('error/', views.error, name="error-page"),
+    # path('error/', views.email_non_exist, name="email-page"),
+    path('tour/muktinath',views.tour_muktinath, name ="muktinath"),
+    path('tour/pathivara',views.tour_pathivara, name ="pathivara"),
+    path('tour/kalinchowk',views.tour_kalinchowk, name ="kalinchowk"),
+    path('tour/ktmpkr',views.tour_ktmpkr, name ="ktmpkr"),
+    path('tour/kailash',views.tour_kailash, name ="kailash"),
+>>>>>>> Stashed changes
 ]
