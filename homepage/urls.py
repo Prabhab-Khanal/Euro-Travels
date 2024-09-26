0dbe63fcc53affc88d8c7d928a5bc3431cd86e67
Saman Gautam <samangautam443@gmail.com>
2024-09-26 19:24:21 +0545

    Removed Backend

--- homepage/urls.py
+++ homepage/urls.py
@@ -6,11 +6,6 @@ urlpatterns =[
     path('',views.index,name='index'),
     path('main/', views.main1, name="main"),
 
-    # for contact me pages
-    path('contact/', views.contact_view, name="contact-me"),
-    path('success/', views.success, name="success-page"),
-    path('error/', views.error, name="error-page"),
-    path('error/', views.email_non_exist, name="email-page"),
     path('tour/muktinath',views.tour_muktinath, name ="muktinath"),
     path('tour/pathivara',views.tour_pathivara, name ="pathivara"),
     path('tour/kalinchowk',views.tour_kalinchowk, name ="kalinchowk"),
